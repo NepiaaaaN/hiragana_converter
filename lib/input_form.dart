a3046db2286d49cd58430350f841857c6b69894d
NepiaaaaN <surface095@gmail.com>
2024-12-12 23:52:05 +0900

    Add input validation

--- lib/input_form.dart
+++ lib/input_form.dart
@@ -8,26 +8,44 @@ class InputForm extends StatefulWidget {
 }
 
 class _InputFormState extends State<InputForm> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
-            maxLines: 5,
-            decoration: InputDecoration(hintText: '文章を入力してください'),
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextFormField(
+              maxLines: 5,
+              decoration: const InputDecoration(
+                hintText: '文章を入力してください',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '文章が入力されていません';
+                }
+                return null;
+              },
+            ),
           ),
-        ),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text(
-            '変換',
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // GlobalKey から Form ウィジェットの State を取得
+              final formState = _formKey.currentState!;
+              // validate メソッドを呼び出すと、Form ウィジェットの子孫にある FormField ウィジェットでバリデーションが行われる
+              formState.validate();
+            },
+            child: const Text(
+              '変換',
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
